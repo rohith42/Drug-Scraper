48e8cd1e4afcf9912827ad057a93c34dace339d8
rohith42 <36945963+rohith42@users.noreply.github.com>
2022-09-06 19:54:25 -0700

    added two main endpoints

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 from flask import *
 import json, time
+from drugDb import DrugDb
 
 app = Flask(__name__)
+db = DrugDb()
 
 @app.route('/', methods = ['GET'])
 def home_page():
     dataset = {'Page': 'Home', 'Message': 'Successfully loaded home page', 'Timestamp': time.time()}
     json_dump = json.dumps(dataset)
-
     return json_dump
 
 
-@app.route('/user', methods = ['GET'])
-def request_page():
-    user = str(request.args.get('usr'))  # /user?usr=USER_NAME
-    
-    dataset = {'Page': 'Request', 'Message': 'Your username is ' + user, 'Timestamp': time.time()}
+@app.route('/allnames', methods = ['GET'])
+def getAllNames():
+    dataset = {'Names': db.getNames()}
     json_dump = json.dumps(dataset)
+    return json_dump
 
+
+@app.route('/drug', methods = ['GET'])
+def getDrugInfo():
+    drugName = str(request.args.get('name'))  # /drug?name=DRUG_NAME
+    dataset = db.getDrug(drugName)
+    json_dump = json.dumps(dataset)
     return json_dump
 
 
